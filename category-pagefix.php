d5905e0c54711f3ec712c0e68136f9c7965f7c03
rahnas <rahnas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-26 13:19:26 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/category-pagination-fix/trunk@113435 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ category-pagefix.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Category pagination fix
+Plugin URI: http://www.htmlremix.com/projects/category-pagination-wordpress-plugin
+Description: Fixes 404 page error in pagination of category page while using custom permalink
+Version: 1.0
+Author: Remiz Rahnas
+Author URI: http://www.htmlremix.com
+
+Copyright 2009  Creative common  (email: mail@htmlremix.com)
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+You are allowed to use, change and redistibute without any legal issues. I am not responsible for any damage caused by this program. Use at your own risk
+Tested with Wordpress 2.7 only. Works with wp-pagenavi
+*/
+
+/**
+ * This plugin will fix the problem where next/previous of page number buttons are broken on list
+ * of posts in a category when the custom permalink string is:
+ * /%category%/%postname%/ 
+ * The problem is that with a url like this:
+ * /categoryname/page/2
+ * the 'page' looks like a post name, not the keyword "page"
+ */
+function remove_page_from_query_string($query_string)
+{ 
+    if ($query_string['name'] == 'page' && isset($query_string['page'])) {
+        unset($query_string['name']);
+        // 'page' in the query_string looks like '/2', so i'm spliting it out
+        list($delim, $page_index) = split('/', $query_string['page']);
+        $query_string['paged'] = $page_index;
+    }      
+    return $query_string;
+}
+// I will kill you if you remove this. I died two days for this line 
+add_filter('request', 'remove_page_from_query_string');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Plug in page : Category pagination fix
+Version : 1.o
+Plugin URL : http://www.htmlremix.com/projects/category-pagination-fix-wordpress-plugin
+Auther name: Remiz Rahnas
+Auther URL : http://www.htmlremix.com
+Release date : 20090425
